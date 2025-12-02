1369e3c20be41273202878af5a3c2fe7b142bb8f
Brian Lucas <brianlucas67@gmail.com>
2025-12-02 14:12:22 -0400

    feat: Add imageSize parameter to ImageConfig and update README example

--- src/Data/ImageConfig.php
+++ src/Data/ImageConfig.php
@@ -15,11 +15,11 @@ final class ImageConfig implements Arrayable
 {
     /**
      * @param string  $aspectRatio  The aspect ratio of the image to generate. Supported aspect ratios: 1:1, 2:3, 3:2, 3:4, 4:3, 9:16, 16:9, 21:9. If not specified, the model will choose a default aspect ratio based on any reference images provided.
-     * @param string $imageSize Specifies the size of generated images. Supported values are 1K, 2K, 4K. If not specified, the model will use default value 1K.
+     * @param string|null $imageSize Specifies the size of generated images. Supported values are 1K, 2K, 4K. If not specified, the model will use default value 1K.
      */
     public function __construct(
         public readonly string $aspectRatio,
-        public readonly string $imageSize,
+        public readonly ?string $imageSize,
     ) {}
 
     /**
